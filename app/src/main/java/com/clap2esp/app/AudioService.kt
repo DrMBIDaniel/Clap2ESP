fbc56c5e2e0b1d77db4693cb9ea8702afd231277
DrMBIDaniel <danyalmurat96@gmail.com>
2026-07-12 21:29:17 +0500

    Update AudioService.kt

--- app/src/main/java/com/clap2esp/app/AudioService.kt
+++ app/src/main/java/com/clap2esp/app/AudioService.kt
@@ -1,10 +1,11 @@
 package com.clap2esp.app
 
-import android.app.Service
+import android.app.*
 import android.content.Intent
 import android.media.AudioFormat
 import android.media.AudioRecord
 import android.media.MediaRecorder
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
 
@@ -12,6 +13,28 @@ class AudioService : Service() {
 
     private var audioRecord: AudioRecord? = null
     private var isRecording = false
+
+    private val channelId = "Clap2ESP_Channel"
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+        createNotificationChannel()
+
+        val notification = Notification.Builder(
+            this,
+            channelId
+        )
+            .setContentTitle("Clap2ESP")
+            .setContentText("Listening for claps...")
+            .setSmallIcon(android.R.drawable.ic_btn_speak_now)
+            .build()
+
+
+        startForeground(1, notification)
+    }
+
 
     override fun onStartCommand(
         intent: Intent?,
@@ -63,16 +86,36 @@ class AudioService : Service() {
 
                 if (read != null && read > 0) {
 
-                    val maxAmplitude = buffer.maxOrNull()
+                    val volume = buffer.maxOrNull()
 
                     Log.d(
                         "CLAP",
-                        "Sound level: $maxAmplitude"
+                        "Volume: $volume"
                     )
                 }
             }
 
         }.start()
+    }
+
+
+    private fun createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val channel = NotificationChannel(
+                channelId,
+                "Clap2ESP Listener",
+                NotificationManager.IMPORTANCE_LOW
+            )
+
+
+            val manager =
+                getSystemService(NotificationManager::class.java)
+
+
+            manager.createNotificationChannel(channel)
+        }
     }
 
 
